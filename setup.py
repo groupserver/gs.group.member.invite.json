6fbcdb140f0465bc9ec2751395a7ee8462d5ef89
wbushey <wbushey@gmail.com>
2013-08-20 13:53:37 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(name='gs.group.member.invite.json',
+    version=version,
+    description="Via JSON, send an invitation to join a group in GroupServer",
+    long_description=open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
+      "Development Status :: 1 - Planning",
+      "Environment :: Web Environment",
+      "Framework :: Zope2",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+      "Natural Language :: English",
+      "Operating System :: POSIX :: Linux"
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='json, invite, invitation, user, profile',
+    author='Bill Bushey',
+    author_email='bill.bushey@e-democracy.org',
+    url='http://e-democracy.org/',
+    license='other',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['edem', 'edem.group', 'edem.group.member',
+                        'edem.group.member.invite'], 
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'gs.group.member.invite.base',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
+
+
