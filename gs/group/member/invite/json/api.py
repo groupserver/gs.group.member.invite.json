f69f01f9e78a432af53f8fd82f14083a6a2495a6
wbushey <wbushey@gmail.com>
2013-08-30 17:55:49 -0500

    What was api_test.py is now api.py
    --HG--
    rename : gs/group/member/invite/json/api_test.py => gs/group/member/invite/json/api.py

--- gs/group/member/invite/json/api.py
+++ gs/group/member/invite/json/api.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
 import json
+from email.utils import parseaddr
 from zope.cachedescriptors.property import Lazy
-from gs.group.base.form import GroupForm
 from zope.formlib import form as formlib
+from gs.content.form.api.json.group_endpoint import GroupEndpoint
+from Products.GSGroup.groupInfo import groupInfo_to_anchor
+from Products.CustomUserFolder.userinfo import userInfo_to_anchor
 from gs.group.member.invite.base.invitefields import InviteFields
-
-# Not sure which of the following are actually required
-from gs.profile.email.base.emailuser import EmailUser
-from Products.GSProfile import interfaces as profileSchemas
-#from processor import JSONProcessor
-# TODO Rename CSVProcessor to JSONProcessor
-# TODO Make JSONProcessor actually process JSON
-#from columns import Columns
-from profilelist import ProfileList
-# TODO Determine if Columns and ProfileList should be in
-# gs.group.member.invite.base
-# TODO Determine if the invite API needs to use Columns and ProfileList
-
-import logging
-log = logging.getLogger('gs.group.member.invite.json')
+from gs.group.member.invite.base.audit import INVITE_NEW_USER, \
+    INVITE_OLD_USER, INVITE_EXISTING_MEMBER
+from processor import InviteProcessor
 
 PROFILE_CREATED = 0
 EXISTING_PROFILE_INVITED = 1
 EXISTING_MEMBER_IGNORED = 2
-VALIDATION_ERROR = 100
 
 
-class InviteUserAPI(GroupForm):
-    # if this is set to true, we invite users. Otherwise we just add them.
-    invite = True
+class InviteUserAPI(GroupEndpoint):
+    label = u'POST data to this URL to invite a member to join this '\
+        + 'group.'
 
     def __init__(self, group, request):
         super(InviteUserAPI, self).__init__(group, request)
@@ -46,196 +36,56 @@ class InviteUserAPI(GroupForm):
         assert retval
         return retval
 
-    @property
-    def invite_only(self):
-        return self.invite
-
-    # TODO Determine if the invite API needs to use profileSchemaName
-    # TODO Determine if profileSchemaName should be in
-    # gs.group.member.invite.base
-    @Lazy
-    def profileSchemaName(self):
-        ifName = self.globalConfiguration.getProperty('profileInterface',
-                                                      'IGSCoreProfile')
-        # --=mpj17=-- Sometimes profileInterface is set to ''
-        ifName = (ifName and ifName) or 'IGSCoreProfile'
-        retval = '%sAdminJoinCSV' % ifName
-        assert hasattr(profileSchemas, ifName), \
-            'Interface "%s" not found.' % ifName
-        assert hasattr(profileSchemas, retval), \
-            'Interface "%s" not found.' % retval
-        return retval
-
-    # TODO Determine if the invite API needs to use profileSchema
-    # TODO Determine if profileSchema should be in
-    # gs.group.member.invite.base
-    @property
-    def profileSchema(self):
-        return getattr(profileSchemas, self.profileSchemaName)
-
-    # TODO Determine if the invite API needs to use profileFields
-    # TODO Determine if profileFields should be in
-    # gs.group.member.invite.base
-    @Lazy
-    def profileFields(self):
-        retval = formlib.Fields(self.profileSchema, render_context=False)
-        return retval
-
-    # TODO Determine if the invite API needs to use profileList
-    # TODO Determine if profileList should be in
-    # gs.group.member.invite.base
-    @Lazy
-    def profileList(self):
-        retval = ProfileList(self.context)
-        return retval
-
-    # TODO Determine if the invite API needs to use requiredColumns
-    # TODO Determine if requiredColumns should be in
-    # gs.group.member.invite.base
-    @Lazy
-    def requiredColumns(self):
-        retval = [p for p in self.profileList if p.value.required]
-        return retval
-
-    # TODO Determine if the invite API needs to use columns
-    # TODO Determine if columns should be in
-    # gs.group.member.invite.base
-    @property
-    def columns(self):
-        retval = []
-
-        profileAttributes = {}
-        for pa in self.profileList:
-            profileAttributes[pa.token] = pa.title
-
-        for i in range(0, len(self.profileList)):
-            j = i + 65
-            columnId = 'column%c' % chr(j)
-            columnTitle = u'Column %c' % chr(j)
-            column = {
-                'columnId': columnId,
-                'columnTitle': columnTitle,
-                'profileList': self.profileList}
-            retval.append(column)
-        assert len(retval) > 0
-        return retval
-
-    @Lazy
-    def adminInfo(self):
-        retval = self.loggedInUserInfo
-        return retval
-
-    @Lazy
-    def fromAddr(self):
-        eu = EmailUser(self.context, self.adminInfo)
-        addrs = eu.get_addresses()
-        retval = addrs[0] if addrs else u''
-        return retval
-
-    # TODO Determine if this should be in gs.group.member.invite.base instead
-    @Lazy
-    def subject(self):
-        retval = u'Invitation to join {0}'.format(self.groupInfo.name)
-        return retval
-
-    # TODO Determine if this should be in gs.group.member.invite.base instead
-    @Lazy
-    def message(self):
-        m = u'''Hi there!
-
-Please accept this invitation to join {0}. I have set everything up for
-you, so you can start participating in the group as soon as you follow
-the link below and accept this invitation.'''
-        retval = m.format(self.groupInfo.name)
-        return retval
-
-    # TODO Make the form return JSON results, not HTML
-    @formlib.action(label=u'Submit', failure='invite_user_failure')
+    @formlib.action(label=u'Submit', prefix='', failure='invite_user_failure')
     def invite_user_success(self, action, data):
-        # TODO Access supplied JSON
-        # TODO Make this mess actually handle an API request with provided JSON
-        #      Also make it less of a mess
         # Zope's regular form validation system *should* take care of checking
         # on columns and what not. So here we just have to pass data on to the
         # actual invite code and package the result up as json
-        retdict = JSONProcessor.process(data)
-        retval = json.dumps(retdict)
+        inviteProcessor = InviteProcessor(self.context, self.request,
+                                          self.siteInfo, self.groupInfo,
+                                          self.loggedInUser, self.form_fields,
+                                          self.inviteFields)
+        result, userInfo = inviteProcessor.process(data)
+
+        # Prep data for display
+        addrName, addr = parseaddr(data['toAddr'].strip())
+        linked_username = userInfo_to_anchor(userInfo)
+        linked_groupname = groupInfo_to_anchor(self.groupInfo)
+        retval = {}
+
+        if result == INVITE_NEW_USER:
+            retval['status'] = 1
+            m = []
+            m.append(u'A profile for {0} has been created, and given the '
+                     u'email address {1}.')
+            m.append(u'{0} has been sent an invitation to join {2}.')
+            m = [i.format(linked_username, addr, linked_groupname) for i in m]
+            retval['message'] = m
+        elif result == INVITE_OLD_USER:
+            retval['status'] = 2
+            m = []
+            m.append(u'Inviting the existing person with the email address '
+                     u'{0} - {1} - to join {2}.')
+            m = [i.format(addr, linked_username, linked_groupname) for i in m]
+            retval['message'] = m
+        elif result == INVITE_EXISTING_MEMBER:
+            retval['status'] = 3
+            m = []
+            m.append(u'The person with the email address {0} - {1} -'
+                     u' is already a member of {2}.')
+            m.append(u'No changes to the profile of {1} have been made.')
+            m = [i.format(addr, linked_username, linked_groupname) for i in m]
+            retval['message'] = m
+        else:
+            retval['status'] = 100
+            retval['message'] = u'An unknown event occurred.'
+
+        retval = json.dumps(retval, indent=4)
         return retval
-
-        # Is anything beyond this comment needed?
-        #form = data
-        #result = {}
-        #result['form'] = form
-
-        #if 'submitted' in form:
-        #    result['message'] = u''
-        #    result['error'] = False
-        #    # FIXME: Fix logging
-        #    #m = u'process_form: Adding users to %s (%s) on %s (%s) in'\
-        #    #  u' bulk for %s (%s)' % \
-        #    #  (self.groupInfo.name,   self.groupInfo.id,
-        #    #   self.siteInfo.get_name(),    self.siteInfo.get_id(),
-        #    #   self.adminInfo.name, self.adminInfo.id)
-        #    #log.info(m)
-
-        #    # Processing the CSV is done in three stages.
-        #    #   1. Process the columns.
-        #    columnProcessor = Columns(self.context, form)
-        #    r = columnProcessor.process()
-        #    result['message'] = u'\n'.join((result['message'], r['message']))
-        #    result['error'] = result['error'] \
-        #        if result['error'] else r['error']
-        #    columns = r['columns']
-        #    processor = CSVProcessor(self.context, self.request, form, columns,
-        #                             self.subject, self.message,
-        #                             self.fromAddr, self.profileSchema,
-        #                             self.profileFields)
-        #    #   2. Parse the file.
-        #    if not result['error']:
-        #        r = processor.process()
-        #        result['message'] = u'\n'.join((result['message'],
-        #                                        r['message']))
-        #        result['error'] = result['error'] or r['error']
-        #        csvResults = r['csvResults']
-        #    #   3. Interpret the data.
-        #    if not result['error']:
-        #        try:
-        #            r = processor.process_csv_results(csvResults,
-        #                                              form['delivery'])
-        #        except UnicodeDecodeError, ude:
-        #            result['error'] = True
-        #            m = u'<p>Error reading the CSV file (did you select the '\
-        #                u'correct file?): <span class="muted">{0}</p></p>'
-        #            result['message'] = m.format(ude)
-        #        else:
-        #            m = u'\n'.join((result['message'], r['message']))
-        #            result['message'] = m
-        #            result['error'] = result['error'] or r['error']
-
-        #    assert 'error' in result
-        #    assert type(result['error']) == bool
-        #    assert 'message' in result
-        #    assert type(result['message']) == unicode
-
-        #assert type(result) == dict
-        #assert 'form' in result
-        #assert type(result['form']) == dict
 
     def invite_user_failure(self, action, data, errors):
-        # Humm... what does errors give us? There *should* be ValidationErrors
-        # which contain messages we can send back to the user.
-        #
-        # Would we want to get fansier and return multiple status/message pairs
-        # for multiple errors? If so, we'd have to create status values for
-        # each type of validation error.
-        retdict = {
-            'status': VALIDATION_ERROR,
-            'message': [unicode(error) for error in errors]
-        }
-        retval = json.dumps(retdict)
-        return retval
+        return self.build_error_response(action, data, errors)
 
     def __call__(self):
-        retval = super(InviteUserAPI, self).__call__()
-        self.request.response.setHeader('Content-Type', 'application/json')
-        return retval
+        log.info(pprint.pformat(self.loggedInUser, indent=2))
+        return super(InviteUserAPI, self).__call__()
