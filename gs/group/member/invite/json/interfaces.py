45c3ebe026f81f760c00d516b8d0d20c1a749b6e
wbushey <wbushey@gmail.com>
2013-08-30 17:58:54 -0500

    Removing unneeded files

--- gs/group/member/invite/json/interfaces.py
+++
@@ -1,7 +0,0 @@
-from zope.interface import Interface
-from zope.schema import Text 
-
-class IApiTest(Interface):
-    foo = Text(title=u'Some Foo',
-                description=u"I don't know what this is",
-                required=False)
